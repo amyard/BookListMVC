02d3aa15fa77ef6c54ced8a0b3cd415d64787e7d
amyard <maksymturenko@gmail.com>
2020-04-11 17:36:08 +0300

    admin. orders. list view + templates

---
+++ BookList/BookList/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BookList.DataAccess.Repository.IRepository;
+using BookList.Models;
+using BookList.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookList.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _uniofWork;
+        public OrderController(IUnitOfWork uniofWork)
+        {
+            _uniofWork = uniofWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // API CALSS
+        [HttpGet]
+        public IActionResult GetOrderList(string status)
+        {
+            // get id of logged user
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            IEnumerable<OrderHeader> orderHeaderList;
+
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                orderHeaderList = _uniofWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                orderHeaderList = _uniofWork.OrderHeader.GetAll(
+                                        u => u.ApplicationUserId == claim.Value,
+                                        includeProperties: "ApplicationUser");
+            }
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaderList = orderHeaderList.Where(o => o.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusApproved ||
+                                                                o.OrderStatus == SD.StatusInProcess ||
+                                                                o.OrderStatus == SD.StatusPending);
+                    break;
+                case "completed":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusShipped);
+                    break;
+                case "rejected":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusCancelled ||
+                                                                o.OrderStatus == SD.StatusRefunded ||
+                                                                o.OrderStatus == SD.PaymentStatusRejected);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaderList });
+        }
+    }
+}
---
+++ BookList/BookList/wwwroot/js/order.js
@@ -0,0 +1,38 @@
+﻿var dataTable;
+
+
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("?status")) {
+        status = url.split("?status=")[1];
+        loadDataTable(`GetOrderList?status=${status}`);
+    }
+});
+
+function loadDataTable(url) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Order/" + url
+        },
+        "columns": [
+            { "data": "id", "width": "10%" },
+            { "data": "name", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/Admin/Order/Details/${data}" class="btn btn-success text-white" style="cursor:pointer;">
+                                <i class="fas fa-edit"></i> &nbsp;
+                            </a>
+                        </div>
+                        `
+                }, "width": "5%"
+            }
+        ]
+    });
+}
